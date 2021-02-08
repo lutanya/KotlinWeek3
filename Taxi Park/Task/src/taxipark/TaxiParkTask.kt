21268f47d7eafed29dbf57f2af53164cb7e48543
Tatsiana Liubetskaya <tatsiana_liubetskaya@epam.com>
2021-02-08 09:57:38 +0300

    task4, task5

--- Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -1,5 +1,7 @@
 package taxipark
 
+import java.util.*
+
 /*
  * Task #1. Find all the drivers who performed no trips.
  */
@@ -8,8 +10,6 @@ fun TaxiPark.findFakeDrivers(): Set<Driver>
         val driversWithTrip=this.trips.map { it.driver.name }
         return this.allDrivers.filter { driver->!driversWithTrip.any { it == driver.name } }.toSet()
     }
-
-
 
 /*
  * Task #2. Find all the clients who completed at least the given number of trips.
@@ -26,26 +26,33 @@ fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger>
  */
 fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger>? {
     val driverToTrips = this.trips.groupBy { it.driver }
-    val listOfPassengers =
-        driverToTrips[driver]?.map { it.passengers }?.flatten()?.groupingBy { it }?.eachCount()?.filter { it.value > 1 }
-    return listOfPassengers?.keys?.toSet()?: emptySet()
+    val mapOfPassengers =
+        flattenSet(driverToTrips[driver])?.filter { it.value > 1 }
+    return mapToSet(mapOfPassengers)
 }
+fun flattenSet(setOfTrips: List<Trip>?)=setOfTrips?.map{it.passengers}?.flatten()?.groupingBy { it }?.eachCount()
 
-
+fun mapToSet(mapOfPassengers:Map<Passenger,Int>?)=mapOfPassengers?.keys?.toSet()?: emptySet()
 /*
  * Task #4. Find the passengers who had a discount for majority of their trips.
  */
 fun TaxiPark.findSmartPassengers(): Set<Passenger>{
-
+val (tripsWithDiscount, tripWithoutDiscount)=this.trips.partition { it.discount != null }
+    val passengersWithoutDiscountTrip=flattenSet(tripWithoutDiscount)
+    val mapOfPassengers = flattenSet(tripsWithDiscount)?.filter{ (key, value) -> !(passengersWithoutDiscountTrip?.containsKey(key)?:false)
+            || (passengersWithoutDiscountTrip?.containsKey(key)?:true && passengersWithoutDiscountTrip?.getValue(key)?:0 < value)}
+    return mapToSet(mapOfPassengers)
 }
-
 
 /*
  * Task #5. Find the most frequent trip duration among minute periods 0..9, 10..19, 20..29, and so on.
  * Return any period if many are the most frequent, return `null` if there're no trips.
  */
 fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
-    return TODO()
+
+    val frduration = this.trips.groupingBy { it.duration/10 }.eachCount().maxByOrNull { it.value }?.key ?: return null
+
+    return frduration*10..frduration*10+9
 }
 
 /*
